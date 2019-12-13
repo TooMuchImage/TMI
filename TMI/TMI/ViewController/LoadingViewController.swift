a5448cc8bc354926a605c816caea5dc2529054f6
부엉이 <o.o.wl.soft@gmail.com>
2019-12-13 21:10:09 +0900

    chore: LoadingViewController 파일 디렉터리 추가

---
+++ TMI/TMI/ViewController/LoadingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingViewController.swift
+//  TMI
+//
+//  Created by 이동영 on 2019/12/13.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
