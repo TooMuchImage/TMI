424626dc18fb5aa66e5d0b4c40a7374f93239221
부엉이 <o.o.wl.soft@gmail.com>
2019-12-14 02:47:25 +0900

    feat: GaugeView Attribute 설정 메소드 추가

--- TMI/TMI/View/GaugeView.swift
+++ TMI/TMI/View/GaugeView.swift
@@ -9,13 +9,46 @@
 import UIKit
 
 class GaugeView: UIView {
+    
+    // MARK: - UI
+    
+    private let filedView = UIView()
+    
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpAttributes()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUpAttributes()
+    }
+}
 
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+// MARK: - Attributes
+
+extension GaugeView {
+    private func setUpAttributes() {
+        self.do {
+            $0.addSubview(filedView)
+            $0.backgroundColor = .loadingGray
+        }
+        
+        filedView.do {
+            $0.backgroundColor = .loadingCyan
+        }
     }
-    */
+}
+
 
 }
