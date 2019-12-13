94fd818a3b27dd5d158573ae63f582b1e7e4d08b
부엉이 <o.o.wl.soft@gmail.com>
2019-12-13 20:51:39 +0900

    feat:  Constraints 제약사항 비율로 적용되게 변경

--- TMI/TMI/ViewController/EntryViewController.swift
+++ TMI/TMI/ViewController/EntryViewController.swift
@@ -61,22 +61,22 @@ extension EntryViewController {
 extension EntryViewController {
     private func setUpConstraints() {
         logoImageView.snp.makeConstraints {
-            $0.width.equalTo(self.view.snp.width).dividedBy(3)
-            $0.height.equalTo(logoImageView.snp.width).multipliedBy(147.0/97.0)
-            $0.top.equalToSuperview().offset(96)
-            $0.leading.equalToSuperview().offset(24)
+            $0.width.equalTo(self.view.safeAreaLayoutGuide).dividedBy(3)
+            $0.height.equalTo(logoImageView.snp.width).aspectRatio(147,97)
+            $0.top.equalToSuperview().inset(self.view.bounds.height * 14/100)
+            $0.leading.equalToSuperview().inset(self.view.bounds.width * 6/100)
         }
         
         guideLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(startButton.snp.top).offset(-100)
+            $0.top.equalToSuperview().inset(self.view.bounds.height * 70/100)
         }
         
         startButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.width.equalTo(126)
-            $0.height.equalTo(48)
-            $0.bottom.equalToSuperview().inset(100)
+            $0.width.equalToSuperview().dividedBy(3)
+            $0.height.equalTo(startButton.snp.width).aspectRatio(48, 126)
+            $0.bottom.equalToSuperview().inset(self.view.bounds.height * 15/100)
         }
     }
 }
