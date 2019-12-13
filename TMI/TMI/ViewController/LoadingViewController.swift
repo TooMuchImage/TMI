c7cc850cca9d7d403dd2cc6a234701c471566fbe
부엉이 <o.o.wl.soft@gmail.com>
2019-12-13 23:39:34 +0900

    feat:  UI컴포넌트 attribute 설정 메소드 추가

--- TMI/TMI/ViewController/LoadingViewController.swift
+++ TMI/TMI/ViewController/LoadingViewController.swift
@@ -26,6 +26,7 @@ class LoadingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpAttributes()
     }
 }
 
@@ -33,7 +34,38 @@ class LoadingViewController: UIViewController {
 
 extension LoadingViewController {
     private func setUpAttributes() {
+        self.view.do {
+            $0.addSubview(logoImageView)
+            $0.addSubview(loadingImageView)
+            $0.addSubview(messageLabel)
+            $0.addSubview(gaugeView)
+            $0.addSubview(gaugeLabel)
+        }
         
+        logoImageView.do {
+            $0.backgroundColor = .gray
+        }
+        
+        loadingImageView.do {
+            $0.backgroundColor = .gray
+        }
+        
+        messageLabel.do {
+            $0.numberOfLines = 2
+            $0.text = "갤러리의 스크린샷을\n정리하고 있어요"
+            $0.font = .boldSystemFont(ofSize: 22)
+            $0.textAlignment = .center
+        }
+        
+        gaugeView.do {
+            $0.backgroundColor = .neonBlue
+        }
+        
+        gaugeLabel.do {
+            $0.text = "20%"
+            $0.font = .systemFont(ofSize: 14)
+            $0.textAlignment = .center
+        }
     }
 }
 
