8540ff80f5deeb057d8bb07f61dabf4d810f4229
부엉이 <o.o.wl.soft@gmail.com>
2019-12-14 02:46:07 +0900

    feat: GaugeView 추가

---
+++ TMI/TMI/View/GaugeView.swift
@@ -0,0 +1,21 @@
+//
+//  GaugeView.swift
+//  TMI
+//
+//  Created by 이동영 on 2019/12/14.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class GaugeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
