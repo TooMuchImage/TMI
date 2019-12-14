d43a28e10f786899cbba46153846f80d66411afa
부엉이 <o.o.wl.soft@gmail.com>
2019-12-15 04:32:21 +0900

    feat: dequeue + type casting 해주는 메소드 추가 확장

--- TMI/TMI/Utilities/Extensions/UICollectionView+.swift
+++ TMI/TMI/Utilities/Extensions/UICollectionView+.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 cmindy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    
+    func dequeueReusableCell<Cell: Reusable>(with type: Cell.Type, for indexPath: IndexPath) -> Cell? {
+        return dequeueReusableCell(withReuseIdentifier: type.reuseIdentifier, for: indexPath) as? Cell
+    }
+    
+    func dequeueReusableHeaderView<Header: Reusable>(of type: Header.Type, for indexPath: IndexPath) -> Header? {
+        return dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
+                                                withReuseIdentifier: Header.reuseIdentifier,
+                                                for: indexPath) as? Header
+    }
+}
+
