c0d004ac82be4d351d8ab8a146de70e32f14ec81
부엉이 <o.o.wl.soft@gmail.com>
2019-12-15 04:32:41 +0900

    feat: AlbumCell 구현

--- TMI/TMI/View/AlbumCell.swift
+++ TMI/TMI/View/AlbumCell.swift
@@ -9,21 +9,94 @@
 import UIKit
 
 class AlbumCell: UICollectionViewCell {
+    
+    // MARK: - UI
+    
+    let titleImageView = UIImageView()
+    let titleLabel = UILabel()
+    let countLabel = UILabel()
+    
+    // MARK: - Initializer
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.contentView.backgroundColor = .neonBlue
+        setUpAttributes()
+        setUpConstraints()
     }
     
     init() {
         super.init(frame: .zero)
         
-        self.contentView.backgroundColor = .neonBlue
+        setUpAttributes()
+        setUpConstraints()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
-        self.contentView.backgroundColor = .neonBlue
+        setUpAttributes()
+        setUpConstraints()
     }
 }
+
+// MARK: - Attributes
+
+extension AlbumCell {
+    private func setUpAttributes() {
+        self.contentView.do {
+            $0.addSubview(titleImageView)
+            $0.addSubview(titleLabel)
+            $0.addSubview(countLabel)
+        }
+        
+        titleImageView.do {
+            $0.backgroundColor = .gray
+            $0.layer.cornerRadius = 8
+        }
+        
+        titleLabel.do {
+            $0.attributedText = ["Instagram", "Facebook", "KakaoTalk"].randomElement()?
+                .beautfiy
+                .attriBeauty
+                .font(.systemFont(ofSize: 16))
+                .align(.left)
+                .apply()
+        }
+        
+        countLabel.do {
+            $0.attributedText = Int.random(in: 0...300).description
+                .beautfiy
+                .attriBeauty
+                .font(.systemFont(ofSize: 14))
+                .fgColor(.lightGray)
+                .align(.left)
+                .apply()
+        }
+    }
+}
+
+// MARK: - Layouts
+
+extension AlbumCell {
+    private func setUpConstraints() {
+        titleImageView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(titleImageView.snp.width)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalTo(titleImageView)
+            $0.top.equalTo(titleImageView.snp.bottom)
+        }
+        
+        countLabel.snp.makeConstraints {
+            $0.leading.trailing.equalTo(titleImageView)
+            $0.top.equalTo(titleLabel.snp.bottom)
+        }
+    }
+}
+
+// MARK: - Reusable
+
+extension AlbumCell: Reusable {}
