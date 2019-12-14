8c6a17ccc3d55f9f9724524630a7e1a74c9a3b61
부엉이 <o.o.wl.soft@gmail.com>
2019-12-15 04:35:16 +0900

    feat: FlowLayoutDeleagate 추가

--- TMI/TMI/ViewController/MainViewController.swift
+++ TMI/TMI/ViewController/MainViewController.swift
@@ -7,17 +7,91 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import AttriBeauty
 
-class MainViewController: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class MainViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    private let albumsCollectionView = UICollectionView(frame: .zero,
+                                                        collectionViewLayout: UICollectionViewFlowLayout())
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpAttributes()
+        setUpConstraints()
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+}
 
+// MARK: - Attributes
+
+extension MainViewController {
+    private func setUpAttributes() {
+        self.view.do {
+            $0.addSubview(albumsCollectionView)
+        }
+        
+        albumsCollectionView.do {
+            $0.dataSource = self
+            $0.delegate = self
+            $0.register(AlbumHeaderView.self,
+                        forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                        withReuseIdentifier: AlbumHeaderView.reuseIdentifier)
+            $0.backgroundColor = .systemGray6
+            $0.register(AlbumCell.self, forCellWithReuseIdentifier: AlbumCell.reuseIdentifier)
+        }
+    }
+}
+
+// MARK: - Layouts
+
+extension MainViewController {
+    private func setUpConstraints() {
+        
+        albumsCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension MainViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(with: AlbumCell.self, for: indexPath) else { return .init() }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableHeaderView(of: AlbumHeaderView.self, for: indexPath) else { return .init()
+        }
+        
+        return header
+    }
+}
 
 // MARK: - UICollectionViewDelegateFlowLayout
 
